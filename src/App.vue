40cac8502c8a290834b3bf09a3cfe6b6d76cacf0
Santiago Mejorada <smejorada@avionica.com>
2023-05-17 10:10:15 -0500

    fix: Update NavBar name import

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 </template>
 <script>
 import StarWars from './components/StarWars.vue';
-import NavBar from './components/navBar.vue';
+import NavBar from './components/NavBar.vue';
 export default {
   name: 'App',
   components: { NavBar, StarWars }
